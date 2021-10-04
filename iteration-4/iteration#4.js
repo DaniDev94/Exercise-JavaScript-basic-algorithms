f840d7d89c8f04c25bc5406a9a10b4869838351d
Daniel López Plaza <daniel.lp1094@gmail.com>
2021-10-04 17:27:05 +0200

    FEAT: iteration#4

---
+++ iteration-4/iteration#4.js
@@ -0,0 +1,31 @@
+/*1.1------------------------------------------------------------------------>*/
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+// console.log(avengers[0]);
+/*1.2------------------------------------------------------------------------>*/
+avengers[0] = "IRONMAN";
+// console.log(avengers);
+/*1.3------------------------------------------------------------------------>*/
+// console.log(avengers.length);
+/*1.4------------------------------------------------------------------------>*/
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+// console.log(rickAndMortyCharacters);
+// console.log(rickAndMortyCharacters[4]);
+/*1.5------------------------------------------------------------------------>*/
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters2.pop();
+// console.log(rickAndMortyCharacters);
+let elementFirstAndLast = rickAndMortyCharacters2[0] + ' ' + rickAndMortyCharacters2[4];
+// console.log(elementFirstAndLast);
+/*1.6------------------------------------------------------------------------>*/
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters3.splice(1,1);//Elimina indicando la posición.
+// console.log(rickAndMortyCharacters3);
+let newArray = rickAndMortyCharacters3.slice(1,5);//Crea una copia de una parte del array indicando la posición. 
+// console.log(newArray);
+
+
+
+
+
+
